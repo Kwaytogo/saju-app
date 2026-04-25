09856f5db119a6377280c5771184ab78af7399f4
Kwaytogo <ronandyjin@gmail.com>
2026-04-25 17:42:26 +0900

    Update SajuFull.jsx

--- components/SajuFull.jsx
+++ components/SajuFull.jsx
@@ -1,4 +1,4 @@
-\import { useState } from "react";
+import { useState } from "react";
 
 const S=['甲','乙','丙','丁','戊','己','庚','辛','壬','癸'];
 const B=['子','丑','寅','卯','辰','巳','午','未','申','酉','戌','亥'];
