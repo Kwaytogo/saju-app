d5144541722313a6ab27799878c0ac484ab22c13
BORN FROM <ronandyjin@gmail.com>
2026-05-09 09:12:12 +0900

    Update route.js

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -1,6 +1,23 @@
-saju-app/app/sitemap.xml
-/route.js
-Kwaytogo
-Kwaytogo
-Create route.js
+export async function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<url>
+<loc>https://bornfrom.co/</loc>
+<lastmod>2026-05-09</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+<url>
+<loc>https://bornfrom.co/legal</loc>
+<lastmod>2026-05-09</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.3</priority>
+</url>
+</urlset>`;
 
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
